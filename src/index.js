927dd793e35afa05e4ab9268809e93315435c0c0
nikita petrov <petrov.nikita702@mail.ru>
2024-04-01 21:43:54 +0300

    решение проблемы с отображением изображений

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./styles/style.css"
 import { Layout, MainPage } from "./content/import_ts"
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import { data } from "./data";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+console.log(window.location.href)
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path='/' element={<Layout header={data.header} footer={data.footer}/>}>
+        <Route path="/" element={<Layout header={data.header} footer={data.footer}/>}>
           <Route index element={<MainPage data={data.mainPage} />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode >
 );
 
